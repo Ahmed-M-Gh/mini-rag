ef8e7bd0040c5c130bf7bf515c9ac922a3e42ee8
Ahmed-M-Gh <gonimeahmed73@gmail.com>
2026-09-16 16:29:35 +0300

    Create the chunking endpoint and make chuks

--- src/routes/data.py
+++ src/routes/data.py
@@ -3,10 +3,11 @@ from fastapi.responses import JSONResponse
 import os 
 import aiofiles
 from src.helpers.config import Settings, get_settings
-from src.controllers import DataController, ProjectController
+from src.controllers import DataController, ProjectController, ProcessController
 from src.models import ResponseSignal
 import logging
 logger = logging.getLogger("uvicorn.error")
+from src.schemes.data import ProcessRequest
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -59,3 +60,34 @@ async def upload_data(
             "file_id" : file_id
         }
     )
+    
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id:str, process_request:ProcessRequest):
+    
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+    
+    process_controller = ProcessController(project_id=project_id)
+    
+    file_content = process_controller.get_file_content(file_id=file_id)
+    if file_content is None:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal" : ResponseSignal.PROCESSING_FAILED.value
+            })
+        
+    
+    
+    file_chunks = process_controller.process_file_content(file_content=file_content, file_id=file_id, chunk_size=chunk_size, overlap_size=overlap_size)
+    
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal" : ResponseSignal.PROCESSING_FAILED.value
+            })
+        
+    return file_chunks
